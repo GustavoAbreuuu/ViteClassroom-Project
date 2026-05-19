8686e7c22563cd523032b25062f4c622a5936c2d
GustavoAbreuuu <gustavo.abreu2009@gmail.com>
2026-05-19 18:36:31 -0300

    chore: migrate styles from css to scss

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import './style.css'
 import * as THREE from 'three';
 
+const canvas = document.querySelector("#experience-canvas");
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
