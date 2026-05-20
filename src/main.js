63e2006e44dfc6984fd9e0da56815c177281618a
GustavoAbreuuu <gustavo.abreu2009@gmail.com>
2026-05-20 06:41:53 -0300

    feat: add window resize handling and centralize dimensions
    - Implement 'resize' event listener to dynamically update camera aspect ratio and renderer size
    - Refactor camera and renderer initialization to use the 'sizes' object consistently

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@ const canvas = document.querySelector("#experience-canvas");
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
-}
+};
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
 
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setAnimationLoop(animate);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -22,6 +22,20 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+
+// Event Listeners
+window.addEventListener("resize", ()=>{
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+// Update Camera
+  camera.aspect = sizes.width / sizes.height; 
+  camera.updateProjectionMatrix()
+
+// Update Renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 function animate(time) {
   cube.rotation.x = time / 2000;
